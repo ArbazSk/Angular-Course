2cdddc5d068e71bf017bad438038d0d59fbab34a
ArbazSk <arbazshaikh5138@gmail.com>
2023-11-18 19:59:23 +0530

    observabels introduction

--- src/app/observables/home/home.component.ts
+++ src/app/observables/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscribable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeObsComponents implements OnInit {
-
+export class HomeObsComponents implements OnInit, OnDestroy {
+  firstSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    this.firstSubscription = interval(1000).subscribe(count => console.log(count));
+  }
+
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();
   }
 
 }
