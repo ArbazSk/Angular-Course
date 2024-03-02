c09507c038c0e7ba11f78a5879be0aacf61a977a
ArbazSk <arbazshaikh5138@gmail.com>
2024-03-02 17:26:10 +0530

    remove api key

--- src/app/project1/auth/auth.service.ts
+++ src/app/project1/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
+import { FIREBASE_API_KEYS } from "env";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
-    private readonly key = "AIzaSyDhLIG-P5Kyh-zndRMe4MsPrDIIPi_JdeU";
+    private readonly key = FIREBASE_API_KEYS;
     private signupURL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + this.key;
     private loginURL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + this.key;
     private http = inject(HttpClient);
