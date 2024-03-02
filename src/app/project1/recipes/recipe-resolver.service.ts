1ce1446f5d9158613a9f325c424cf467c1b84a7e
ArbazSk <arbazshaikh5138@gmail.com>
2024-03-02 15:34:19 +0530

    recipe saved not getting saved because of resovler gets new data

--- src/app/project1/recipes/recipe-resolver.service.ts
+++ src/app/project1/recipes/recipe-resolver.service.ts
@@ -2,10 +2,17 @@ import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "src/app/shared/data-storage.service";
 import { inject } from "@angular/core";
+import { RecipeService } from "./recipe.service";
 
 export const recipeResolver: ResolveFn<Recipe[]> = (
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
 ) => {
-    return inject(DataStorageService).fetchRecipe();
+    const recipeService = inject(RecipeService);
+    const recipe = recipeService.getRecipes();
+    if (recipe.length === 0) {
+        return inject(DataStorageService).fetchRecipe();
+    } else {
+        return recipe;
+    }
 };
