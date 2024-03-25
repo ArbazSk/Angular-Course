2f59da90dd289d9c66922481881c4127253d49a9
ArbazSk <arbazshaikh5138@gmail.com>
2024-03-25 15:57:36 +0530

    created Standalone Component

--- src/app/standalone-component/standalone-component.module.ts
+++ src/app/standalone-component/standalone-component.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from "@angular/core";
 import { StandaloneComponentComponent } from "./standalone-component.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
-import { DetailsComponent } from "./welcome/details/details.component";
-import { RouterModule, Routes } from "@angular/router";
 import { StandaloneComponentRouterModule } from "./standalone-component.routing";
+import { DetailsComponent } from "./welcome/details/details.component";
 
 @NgModule({
     declarations: [
         StandaloneComponentComponent,
         WelcomeComponent,
-        DetailsComponent,
     ],
-    imports: [StandaloneComponentRouterModule]
+    imports: [StandaloneComponentRouterModule, DetailsComponent]
 })
 export class StandaloneComponentModule {
 
